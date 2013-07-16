0889e8f0f435eef838174a42ec47901b8a28f73c
Bill Wacek <wacek@yahoo.com>
2013-07-16 19:30:59 -0400

    Adding basic files created from Rails command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JoshTalks::Application.config.secret_key_base = '477f13212012171c7d0b4aa12e51202eb77089f2c70eeae192b87049c6fdfb9e0e4742a050069db28c60b23b20f01df7b7fe432e6940eb6e818c12245c4f0f25'
